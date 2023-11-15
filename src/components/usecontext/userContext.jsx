2d725bd781fe43384e30666d60e69782a4dff3b9
kavita <ykittu786@gmail.com>
2023-11-15 17:42:28 +0530

    complete hooks practice

---
+++ src/components/usecontext/userContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const userData = {
+    name: "kavita yadav",
+    age: 26,
+  };
+
+  return <AppContext.Provider value={userData}>{children}</AppContext.Provider>;
+};
+
+// custom hook
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider, useGlobalContext };
